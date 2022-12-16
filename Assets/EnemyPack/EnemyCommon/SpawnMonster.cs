5c9e3dd67cd2c58a9b2b40d81fbf3e9a9534aa6b
Rickyeeeeee <64359824+Rickyeeeeee@users.noreply.github.com>
2022-12-16 22:46:13 +0800

    new

---
+++ Assets/EnemyPack/EnemyCommon/SpawnMonster.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMonster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject monster1;
+    public GameObject monster2;
+    public GameObject monster3;
+    public GameObject monster4;
+
+    // public void Spawn()
+
+}
